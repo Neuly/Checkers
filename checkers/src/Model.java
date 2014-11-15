778085ddccf78b2828cb7054ccd40410df5450c2
P. Neulinger <pneuling@cosy.sbg.ac.at>
2014-11-15 15:48:14 +0100

    Problems with checkTokenPosition

--- checkers/src/Model.java
+++ checkers/src/Model.java
@@ -12,9 +12,17 @@ public class Model {
 	/** number of tokens on each side */
 	private static final int NUM_PLAYER = 8;
 
+	/** playing the game on black fields (default) */
+	private boolean BLACK;
+
+	/** playing the game on white fields */
+	private boolean WHITE;
+
 	/** The construktor of the model */
 	public Model() {
 		board = new Token[8][8];
+		BLACK = true;
+		WHITE = false;
 		// fills the board on each side with board
 		// stops if _n is as big as NUM_PLAYER
 
@@ -36,7 +44,7 @@ public class Model {
 		/* fills one side of the board with tokens */
 		for (int y = 0; y < 3 && _n < NUM_PLAYER; y++)
 			for (int x = 0; x < 8 && _n < NUM_PLAYER; x++)
-				if ((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
+				if (checkValidField(x, y)) {
 					board[x][y].setPlayer(Model.PLAYER_ONE);
 					_n++;
 				}
@@ -45,7 +53,7 @@ public class Model {
 		_n = 0;
 		for (int y = 7; y > 4 && _n < NUM_PLAYER; y--)
 			for (int x = 0; x < 8 && _n < NUM_PLAYER; x++)
-				if ((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
+				if (checkValidField(x, y)) {
 					board[x][y].setPlayer(Model.PLAYER_TWO);
 					_n++;
 				}
@@ -73,22 +81,37 @@ public class Model {
 		// if it is in those borders, it is valid
 		// it is also not allowed, that theire is a button at this position
 		// also the field needs too be at a spedified field
-		if (i_pos >= 0
-				&& i_pos <= 7
-				&& j_pos >= 0
-				&& j_pos <= 7
-				&& ((i_pos % 2 == 0 && j_pos % 2 == 0) || (i_pos % 2 == 1 && j_pos % 2 == 1))) {
-
-			
+		if (i_pos >= 0 && i_pos <= 7 && j_pos >= 0 && j_pos <= 7
+				&& checkValidField(i_pos, j_pos)) {
 
 			board[i_pos][j_pos].setPressed(false);
 		} else {
-
+			mov.setBack();
+			i_pos = (int) map(mov.getX(), 0, View.HEIGHT, -1, 8);
+			j_pos = (int) map(mov.getY(), 0, View.WIDTH, -1, 8);
+			
+			board[i_pos][j_pos] = mov;
+			System.out.println("here " + i_pos + " " + j_pos);
 		}
 		System.out.println("validoutside");
 
 	}
 
+	/**
+	 * returns true, if the current fiel is valid according to the color
+	 * representation of the field
+	 */
+	private boolean checkValidField(int i_pos, int j_pos) {
+		if (((i_pos % 2 == 0 && j_pos % 2 == 0) || (i_pos % 2 == 1 && j_pos % 2 == 1))
+				&& BLACK)
+			return true;
+		if (((i_pos % 2 == 1 && j_pos % 2 == 0) || (i_pos % 2 == 0 && j_pos % 2 == 1))
+				&& WHITE)
+			return true;
+		return false;
+	}
+
+	/** the mapping function - copied from the process documentation */
 	public final float map(float value, float istart, float istop,
 			float ostart, float ostop) {
 		return ostart + (ostop - ostart)
